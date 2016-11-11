e62f3aef1a9cc8a6409adcaf574fbb8c283ad627
zhangwentao <zhangwentao@qtone.cn>
2016-11-11 09:50:03 +0800

    删除不必要的库和组建，修改优化细节表现

--- js/qky_ui3.2.js
+++ js/qky_ui3.2.js
@@ -21,21 +21,6 @@
 **************************************************************/
 
 
-/****************************************************************************整体公用*************/
-
-//底部自动漂浮在最低
-function footinbottom(header,content,foot){
-	var body_h=$(content).outerHeight()+$(header).outerHeight()+$(foot).outerHeight();
-	if($(window).height()>=body_h){
-		var foot_mt=$(window).height()-body_h;
-	  $(foot).css("margin-top",foot_mt+"px");
-	}else{
-		$(foot).css("margin-top","16px");
-	}
-	$(content).css({"margin-top":$(header).outerHeight()+"px"});
-}	
-
-
 
 /****************************************************************************下拉菜单（日历插件单独）*************/
 
